10677af3d821046e1ae2d96fd3dceb005983e528
SANDY HADIYA <sandyhadiya@gmail.com>
2021-10-23 13:50:22 +0530

    shop by color added

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    console.log("hii--1");
     $(".review-wrap").slice(0, 1).show();
     $(".mobile_btn").on("click", function(e){
         e.preventDefault();
@@ -7,6 +6,16 @@ $(document).ready(function(){
        $(".review-wrap:hidden").slice(0, 1).slideDown();
         if($(".review-wrap:hidden").length == 0) {
           $(".mobile_btn").fadeOut("slow");
+        }
+      });
+
+
+      $(".shop-by-color-item").slice(0, 2).show();
+      $(".shop-by-color-link").on("click", function(e){
+        e.preventDefault();
+        $(".shop-by-color-item:hidden").slice(0, 2).slideDown();
+        if($(".shop-by-color-item:hidden").length == 0) {
+           $(".shop-by-color-link").fadeOut("slow");
         }
       });
   });   
